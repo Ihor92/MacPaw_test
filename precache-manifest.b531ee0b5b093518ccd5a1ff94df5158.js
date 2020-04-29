767b206679a4992fe5ffd21cd3ec3e124b073d3a
Ihor92 <salfetnikov92@gmail.com>
2020-04-29 16:53:50 +0300

    Updates

---
+++ precache-manifest.b531ee0b5b093518ccd5a1ff94df5158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2808d648dde50c7e51a86426df2f24",
+    "url": "/mac-paw_test/index.html"
+  },
+  {
+    "revision": "51d5790e79cc5ea284fe",
+    "url": "/mac-paw_test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ca74ea704943b59b655a",
+    "url": "/mac-paw_test/static/js/2.7a1440d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mac-paw_test/static/js/2.7a1440d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d5790e79cc5ea284fe",
+    "url": "/mac-paw_test/static/js/main.2ced912d.chunk.js"
+  },
+  {
+    "revision": "5e6081fc5eab8805c207",
+    "url": "/mac-paw_test/static/js/runtime-main.d36bc574.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mac-paw_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
